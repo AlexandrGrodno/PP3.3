a10ea2bf1d7d253cf118835c84ab6065893d777d
AlexandrGrodno <avgust_sa@mail.ru>
2024-05-04 23:21:21 +0300

    запустился

--- src/main/resources/static/info.js
+++ src/main/resources/static/info.js
@@ -25,6 +25,39 @@ function  currentUser() {
 
         })
 }
+function buttonEdit(id){
+    console.log("sadsadas"+id);
+    console.log("нажата")
+
+    fetch("http://localhost:8080/admin/user/" + id)
+        .then(response => response.json())
+        .then(user => {
+            document.getElementById('formGroupIdInput2').value = user.id;
+            document.getElementById('formGroupFirstNameInput2').value = user.userName;
+            document.getElementById('formGroupLastNameInput2').value = user.lastName;
+            document.getElementById('formGroupAgeInput2').value = user.age;
+            document.getElementById('InputEmail2').value = user.email;
+            document.getElementById('formGroupPasswordInput2').value = user.password;
+        });
+}
+
+function buttonDelete(id){
+    console.log("нажата")
+    fetch("http://localhost:8080/admin/user/" +id)
+        .then(response => response.json())
+        .then(user => {
+            // document.getElementById('#formGroupIdInput2').value = user.id;
+
+            let delModal = document.getElementById('formModal');
+            delModal.id.value = user.id;
+            delModal.username.value = user.userName;
+            delModal.lastname.value = user.lastName;
+            delModal.age.value = user.age;
+            delModal.email.value = user.email;
+            delModal.password.value = user.password;
+
+        });
+}
 function creatTable(){
     let table = document.getElementById('allUser');
     let row ='';
@@ -44,19 +77,16 @@ function creatTable(){
                             <td> ${user.email}</td>
                             <td> ${user.role}</td>
                             <td><button type = "button" 
-                                        name="edit"
                                         id= "editUserBtn" 
-                                                                                     
+                                        onclick="buttonEdit(${user.id})"                                            
                                         class="btn btn-primary" 
                                         data-bs-toggle = "modal" 
-                                        data-bs-target = "#editModal" 
-                                        th:attr ="${user.id}"
-                                        userid="${user.id}" > Edit </button> </td>
-                            <td><button type = "button" id = "deleteUserBtn" 
-                                class="btn btn-danger" data-bs-toggle = "modal" 
-                                data-bs-target = "#deleteModal" 
-                                th:attr = "${user.id}"
-                                userid="${user.id}" > delete </button> </td>
+                                        data-bs-target = "#editModal"> Edit </button> </td>
+                            <td><button type = "button" 
+                                        id = "deleteUserBtn" 
+                                        onclick="buttonDelete(${user.id})"  
+                                        class="btn btn-danger" data-bs-toggle = "modal" 
+                                        data-bs-target = "#deleteModal"> delete </button> </td>
                       </tr>`;
 
 
